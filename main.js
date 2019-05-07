4b3af4d6b9bc00dd5de3aa64f22fa213ce8fd28a
Aldane Purrier <aldanepur@gmail.com>
2019-05-07 12:57:21 -0400

    challenges

---
+++ main.js
@@ -0,0 +1,20 @@
+for (x = 0; x < 100; x++) {
+  // console.log(x + 1)
+ if (x % 3 == 0 && x % 5 == 0) {
+   console.log("FizzBuzz")
+
+ } else if (x % 3 == 0) {
+   console.log("Fizz")
+
+ } else if ( x % 5 == 0) {
+   console.log ("Buzz")
+
+ } else {
+   console.log(x)
+
+ }
+
+ }
+
+// I didn't include the first reverse loop and the Pig Latin loop because I want
+ to further my understanding
